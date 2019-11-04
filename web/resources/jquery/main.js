92146f40dd2fa62003ad390b154a8b7348dbf430
ejller <ejller@bk.ru>
2019-11-04 13:52:58 +0300

    some parallax shit

--- web/resources/jquery/main.js
+++ web/resources/jquery/main.js
@@ -2,18 +2,19 @@ $ = jQuery;
 $(window).scroll(function(){
 
     var st = $(this).scrollTop();
+    console.log(st);
+        $("#test").css({
+            "transform": "translate( 0%, " + (st / 10) + "%"
+        })
 
-   $("#test").css({
-       "transform" : "translate( 0%, " + (st/10) + "%"
-    })
+        $("#first_parallax").css({
+            "transform": "translate( 0%, -" + (st / 20) + "%"
+        })
+        $("#sec_parallax").css({
+            "transform": "translate( 0%, -" + (st / 10) + "%"
+        })
+        $("#third_parallax").css({
+            "transform": "translate( 0%, -" + (st / 9.98) + "%"
+        });
 
-    $("#first_parallax").css({
-        "transform" : "translate( 0%, -" + (st/10) + "%"
-    })
-    $("#sec_parallax").css({
-        "transform" : "translate( 0%, -" + (st/5) + "%"
-    })
-    $("#third_parallax").css({
-        "transform" : "translate( 0%, -" + (st/3) + "%"
-    })
 });
