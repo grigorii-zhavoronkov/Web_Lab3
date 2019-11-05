59cc28067934d5dd04f7fe1c597181c06f8f2519
ejller <ejller@bk.ru>
2019-11-06 02:19:33 +0300

    Front develop v.3

--- web/resources/js/main.js
+++ web/resources/js/main.js
@@ -1,9 +1,9 @@
 const zero = 200;
-let r_now;
+let r_now=1;
 const r_const = 48;
 document.addEventListener("DOMContentLoaded", function(event) {
     getValueCanvas();
-    drawBatman(1);
+    drawBatman(r_now);
 });
 
 function drawBatman (r) {
@@ -12,7 +12,6 @@ function drawBatman (r) {
     context.clearRect(0,0,400,400);
     let kof = (4 - r)*0.25*400;
     r_now = r;
-    //console.log(r);
     context.drawImage(document.getElementById('batman'), kof/2, kof/2, 400-kof, 400-kof);
 
 }
@@ -50,11 +49,50 @@ function getValueCanvas() {
                  let new_x = document.getElementById("j_idt7:X:"+(4+x));
                  console.log(x);
                  new_x.checked = true;
-                 //document.forms["j_idt7"].submit();
                  document.getElementById('j_idt7:submit').click();
              }
-            //console.log("x "+x/r+"y "+y/r+" r "+r);
          }
 
      }
  }
+
+ //метод рисует точку по данным
+
+function drawPoint(correct, isIn, x, y ,r) {
+    let xCanvas = 0;
+    let yCanvas = 0;
+    if (correct!=='0'){
+        if (x<0){
+            xCanvas = 200 - Math.abs(x)*r*48;
+        } else {
+
+            xCanvas = 200 + x*r*48;
+        }
+        if (y<0){
+            yCanvas = 200 + Math.abs(y)*r*480;
+        } else {
+            yCanvas = 200 - y*r*48;
+        }
+
+        pointDrawCanvas(isIn, xCanvas,yCanvas, r);
+    }
+}
+
+function pointDrawCanvas(isIn, xCanvas, yCanvas,r ) {
+    let canvas = document.getElementById('paintCanvas');
+    if (canvas.getContext) {
+        let ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBatman(r);
+        ctx.beginPath();
+
+        if (isIn!==0){
+            ctx.fillStyle = "#470005";}
+        else {
+            ctx.fillStyle = "rgba(91,234,65,0.68)";
+        }
+        ctx.moveTo(125, 35);
+        ctx.arc(xCanvas, yCanvas, 6, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
